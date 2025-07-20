4a4bc96095dff6feb9dc340091b90f327f99da87
Aman Gaiola <amngairola@gmail.com>
2025-07-20 21:03:41 +0530

    dataLoader-setup

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,15 @@ import Home from "./pages/Home";
 import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import NotFound from "./pages/NotFound";
-import JobPage from "./pages/JobPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<Home />} />
       <Route path={"/jobs"} element={<JobsPage />} />
+      <Route path={"/jobs/:id"} element={<JobPage />} loader={jobLoader} />
       <Route path={"*"} element={<NotFound />} />
-      <Route path={"/jobs/:id"} element={<JobPage />} />
     </Route>
   )
 );
